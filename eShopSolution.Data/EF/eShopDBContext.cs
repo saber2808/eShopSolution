29f67a11d5a01319a55b9616a7eac625c1ab37bc
Đức Huy <79628064+saber2808@users.noreply.github.com>
2021-12-11 13:09:40 +0700

    Update Entity

---
+++ eShopSolution.Data/EF/eShopDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopDBContext : DbContext
+    {
+        public eShopDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CategoriesTranslation> CategoriesTranslations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+    }
+}
